5411f7275edfeb8309642d62af2e8d27fa80fe21
Luiz Furtado <luizfpfurtado@hotmail.com>
2020-12-03 21:56:13 -0300

    Fase 02-03, Iniciando Backend: added data file and dinamic data on courses and about pages

---
+++ fase02-03/01-iniciando-backend/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    id: "cZvWDTqTGKg",
+    title: "Quadro de imagens igual ao Pinterest com CSS puro",
+    duration: "14:13 minutos",
+    price: "Free"
+  },
+  {
+    id: "p34kaxW0WjU",
+    title: "Sistema admin em 12 minutos com Adminbro",
+    duration: "21:57 minutos",
+    price: "Free"
+  },
+  {
+    id: "XlNLjG2c9uM",
+    title: "Leitura de textos com Speechsynthesis",
+    duration: "11:19 minutos",
+    price: "Free"
+  },
+  {
+    id: "r8v-eSWrvc0",
+    title: "Organização de código com Design Pattern Decorator",
+    duration: "48:47 minutos",
+    price: "Free"
+  },
+  {
+    id: "-RWPvVcYAC4",
+    title: "HTML5 Canvas e JavaScript para manipular imagens",
+    duration: "85:04 minutos",
+    price: "Free"
+  },
+  {
+    id: "K5yYBJhix5A",
+    title: "Extraindo dados de outros sites com Puppeteer JS",
+    duration: "26:50 minutos",
+    price: "Free"
+  }
+]
--- fase02-03/01-iniciando-backend/server.js
+++ fase02-03/01-iniciando-backend/server.js
@@ -2,19 +2,41 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const videos = require('./data');
 server.use(express.static('public'));
 server.set('view engine', 'njk');
 
 nunjucks.configure('views', {
-  express: server
+  express: server,
+  autoescape: false
 });
 
 server.get('/', function(req, res) {
-  return res.render('about');
+  const data = {
+    avatar_url: "https://avatars3.githubusercontent.com/u/19859764?s=460&u=c3af343e6639c6f89dd3deb12c98a2fcaf111921&v=4",
+    name: "Luiz Furtado",
+    role: "Fullstack Developer",
+    bio: "Desenvolvedor Fullstack com ótimos conhecimentos em Javascript e C#. Apaixonado por novas tecnologias e por transformar essas tecnologias em soluções para os problemas das pessoas.",
+    social: [
+      {
+        name: "Github",
+        url: "https://github.com/LuizFurtado"
+      },
+      {
+        name: "Linkedin",
+        url: "https://www.linkedin.com/in/luiz-fernando-furtado/"
+      },
+      {
+        name: "Twitter",
+        url: "https://twitter.com/luizfpfurtado"
+      }
+    ]
+  }
+  return res.render('about', { info: data });
 });
 
 server.get('/portfolio', function(req, res) {
-  return res.render('portfolio');
+  return res.render('portfolio', { items: videos });
 });
 
 server.listen(5000, function() {
