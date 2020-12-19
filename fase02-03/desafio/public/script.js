b230ed481764ac4befe0a7f8ed001e897688f848
Luiz Furtado <luizfpfurtado@hotmail.com>
2020-12-19 17:57:45 -0300

    Final desafio project - added route to display blog posts on another page. Modal overlay disabled

--- fase02-03/desafio/public/script.js
+++ fase02-03/desafio/public/script.js
@@ -5,18 +5,6 @@ const modal = document.querySelector('.modal');
 for (let card of cards) {
   card.addEventListener('click', function() {
     const postId = card.getAttribute('id');
-    const post = modalOVerlay.querySelector('iframe');
-    post.src = `https://blog.rocketseat.com.br/${postId}`;
-    modalOVerlay.classList.add('active');
-    modal.classList.remove('full-screen');
+    window.location.href = `/post/${postId}`;
   });
 }
-
-document.querySelector('.open-in-full-modal').addEventListener('click', function() {
-  modal.classList.toggle('full-screen');
-});
-
-document.querySelector('.close-modal').addEventListener('click', function() {
-  modalOVerlay.classList.remove('active');
-  modalOVerlay.querySelector('iframe').src = "";
-});
--- fase02-03/desafio/server.js
+++ fase02-03/desafio/server.js
@@ -7,7 +7,8 @@ server.use(express.static('public'));
 server.set('view engine', 'njk');
 
 nunjucks.configure('views', {
-  express: server
+  express: server,
+  noCache: true
 });
 
 server.get('/', function(req, res) {
@@ -16,6 +17,17 @@ server.get('/', function(req, res) {
 
 server.get('/content', function(req, res) {
   return res.render('courses', { posts });
+});
+
+server.get('/post/:id', function(req,res) {
+  const { id } = req.params;
+  const post  = posts.find(function(post) {
+    return post.id === id;
+  });
+
+  if(!post) return res.send("Ooopps... Conteúdo não encontrado!");
+
+  return res.render('post', { post });
 });
 
 server.use(function(req, res) {
